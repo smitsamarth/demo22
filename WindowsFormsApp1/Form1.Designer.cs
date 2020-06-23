97dc50a6aa34fa195e324daea2950ede0e736b55
Smit <smitsamarth1989@gmail.com>
2020-06-23 01:08:17 -0400

    gy

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tablesmitBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.smitdbDataSet = new WindowsFormsApp1.smitdbDataSet();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tablesmitTableAdapter = new WindowsFormsApp1.smitdbDataSetTableAdapters.TablesmitTableAdapter();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.areaidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoryidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.areanameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.areaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet = new WindowsFormsApp1._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet();
+            this.button2 = new System.Windows.Forms.Button();
+            this.areaTableAdapter = new WindowsFormsApp1._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSetTableAdapters.AreaTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tablesmitBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smitdbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.areaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tablesmitBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(75, 8);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(374, 167);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // tablesmitBindingSource
+            // 
+            this.tablesmitBindingSource.DataMember = "Tablesmit";
+            this.tablesmitBindingSource.DataSource = this.smitdbDataSet;
+            // 
+            // smitdbDataSet
+            // 
+            this.smitdbDataSet.DataSetName = "smitdbDataSet";
+            this.smitdbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click Here";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tablesmitTableAdapter
+            // 
+            this.tablesmitTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.areaidDataGridViewTextBoxColumn,
+            this.categoryidDataGridViewTextBoxColumn,
+            this.areanameDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.areaBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(75, 244);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(618, 212);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // areaidDataGridViewTextBoxColumn
+            // 
+            this.areaidDataGridViewTextBoxColumn.DataPropertyName = "area_id";
+            this.areaidDataGridViewTextBoxColumn.HeaderText = "area_id";
+            this.areaidDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.areaidDataGridViewTextBoxColumn.Name = "areaidDataGridViewTextBoxColumn";
+            this.areaidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // categoryidDataGridViewTextBoxColumn
+            // 
+            this.categoryidDataGridViewTextBoxColumn.DataPropertyName = "category_id";
+            this.categoryidDataGridViewTextBoxColumn.HeaderText = "category_id";
+            this.categoryidDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.categoryidDataGridViewTextBoxColumn.Name = "categoryidDataGridViewTextBoxColumn";
+            // 
+            // areanameDataGridViewTextBoxColumn
+            // 
+            this.areanameDataGridViewTextBoxColumn.DataPropertyName = "area_name";
+            this.areanameDataGridViewTextBoxColumn.HeaderText = "area_name";
+            this.areanameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.areanameDataGridViewTextBoxColumn.Name = "areanameDataGridViewTextBoxColumn";
+            // 
+            // areaBindingSource
+            // 
+            this.areaBindingSource.DataMember = "Area";
+            this.areaBindingSource.DataSource = this._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet;
+            // 
+            // _C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet
+            // 
+            this._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet.DataSetName = "_C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet";
+            this._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 462);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 67);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Find Area";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // areaTableAdapter
+            // 
+            this.areaTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(730, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1296, 539);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Startup Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tablesmitBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smitdbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.areaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private smitdbDataSet smitdbDataSet;
+        private System.Windows.Forms.BindingSource tablesmitBindingSource;
+        private smitdbDataSetTableAdapters.TablesmitTableAdapter tablesmitTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private _C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet _C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet;
+        private System.Windows.Forms.BindingSource areaBindingSource;
+        private _C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSetTableAdapters.AreaTableAdapter areaTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn areaidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoryidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn areanameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the '_C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet.Area' table. You can move, or remove it, as needed.
+            this.areaTableAdapter.Fill(this._C__USERS_PRATIK_DESKTOP_JOBADDA_APP_DATA_JOBPORTALDB_MDFDataSet.Area);
+            // TODO: This line of code loads data into the 'smitdbDataSet.Tablesmit' table. You can move, or remove it, as needed.
+            this.tablesmitTableAdapter.Fill(this.smitdbDataSet.Tablesmit);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["smitdbConnectionString"].ToString());
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-QK3E55E\SMITSQL;Initial Catalog=smitdb;Integrated Security=True");
+            SqlDataAdapter da;
+            DataTable ds;
+            string qry = "select * from Tablesmit";
+            da = new SqlDataAdapter(qry, con);
+            ds = new DataTable();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Area Updated";
+
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-QK3E55E\SMITSQL;Initial Catalog=HRMSdb;Integrated Security=True");
+            SqlDataAdapter da;
+            DataTable ds;
+
+            DataTable dsnew;
+
+
+            string qry = "select * from area";
+            da = new SqlDataAdapter(qry, con);
+            ds = new DataTable();
+            da.Fill(ds);
+
+            dsnew = ds;
+
+            dataGridView2.DataSource = ds;
+        }
+    }
+}
